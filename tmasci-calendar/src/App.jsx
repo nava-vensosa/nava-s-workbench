20ed726a2be918d323126b863d99880e47107e12
Novalynn <nava.vensosa@gmail.com>
2025-06-17 12:35:12 -0700

    Update App.jsx

---
+++ tmasci-calendar/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to tmasci-calendar!</h1>
+      <p>Your React app is set up and ready to go.</p>
+    </div>
+  );
+}
+
+export default App;
